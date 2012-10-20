736194d80305d12d269e0b13e376f2caaebae29f
Nilam Doctor <nilamdoc@gmail.com>
2012-10-20 13:47:46 +0530

    Commit

---
+++ app/views/elements/header.html.php
@@ -0,0 +1,32 @@
+    <!-- Navbar
+    ================================================== -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="./index.html">Application Name</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-th-list icon-white"></i>&nbsp;App<b class="caret"></b></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#"><i class="icon-th"></i>&nbsp;Menu 1</a></li>
+						<li><a href="#"><i class="icon-th"></i>&nbsp;Menu 2</a></li>				
+						<li><a href="#"><i class="icon-th"></i>&nbsp;Menu 3</a></li>				
+					</ul>
+				</li>	
+            </ul>
+          </div>
+			<?php
+		  	$search = array("url"=>"/Search", "class"=>"navbar-search pull-right form-search form-inline");
+			echo $this->form->create(null,$search);
+			echo $this->form->field('SearchText', array("value"=>$_REQUEST['SearchText'],"class"=>"search-query span2","placeholder"=>"Search","label"=>"")); 
+			echo $this->form->end();
+			?>
+        </div>
+      </div>
+    </div>
--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -10,22 +10,43 @@
 <html>
 <head>
 	<?php echo $this->html->charset();?>
-	<title>Application &gt; <?php echo $this->title(); ?></title>
-	<?php echo $this->html->style(array('debug', 'lithium')); ?>
-	<?php echo $this->scripts(); ?>
+	<title><?php echo $this->title(); ?></title>
+	<?php echo $this->html->style(array('/bootstrap/docs/assets/css/bootstrap')); ?>
+	<?php echo $this->html->style(array('/bootstrap/docs/assets/css/bootstrap-responsive')); ?>	
+	<?php echo $this->html->style(array('/bootstrap/docs/assets/css/docs')); ?>	
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
+
+	<?php
+	$this->scripts('<script src="/bootstrap/docs/assets/js/jquery.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/application.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-affix.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-alert.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-button.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-carousel.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-collapse.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-dropdown.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-modal.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-popover.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-scrollspy.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-tab.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-tooltip.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-transition.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap-typeahead.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap.js"></script>'); 
+	$this->scripts('<script src="/bootstrap/docs/assets/js/bootstrap.min.js"></script>'); 
+
+	?>   		
+	<?php echo $this->scripts(); ?>
 </head>
-<body class="app">
+<body data-spy="scroll" data-target=".bs-docs-sidebar">
 	<div id="container">
 		<div id="header">
-			<h1>Application</h1>
-			<h2>
-				Powered by <?php echo $this->html->link('Lithium', 'http://lithify.me/'); ?>.
-			</h2>
+			<?	echo $this->_render('element', 'header');?>
 		</div>
 		<div id="content">
 			<?php echo $this->content(); ?>
 		</div>
 	</div>
+
 </body>
 </html>
